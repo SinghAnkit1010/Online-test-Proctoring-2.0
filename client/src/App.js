22ee69ffb193b52d858109026dde4545988c7875
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-11-02 00:02:30 +0530

    client create

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-orange-800 text-lg">Fuck Off</h1>
+    </div>
+  );
+}
+
+export default App;
