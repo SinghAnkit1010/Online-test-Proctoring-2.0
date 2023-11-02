a1299b0a55ad8bac8de48db0e8e7c893a9a227e6
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-11-03 02:04:09 +0530

    layout wrap

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Avatar, Badge, message } from 'antd';
 import Footer from './Footer';
 import { useState } from 'react';
 import ConfirmationModal from './ConfirmationPage';
-import { hideLoading } from "../redux/features/alertSlice"
+import { hideLoading } from '../redux/features/alertSlice';
 
 const Layout = ({ children }) => {
   const navigate = useNavigate();
@@ -12,7 +12,7 @@ const Layout = ({ children }) => {
   const location = useLocation();
 
   const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false);
-  // const { user } = useSelector((state) => state.users);
+  const { user } = useSelector((state) => state.user);
 
   const showLogoutConfirmationModal = () => {
     setShowLogoutConfirmation(true);
@@ -34,86 +34,83 @@ const Layout = ({ children }) => {
     setShowLogoutConfirmation(false);
   };
 
-
   return (
-    <>
-      <div className="flex justify-between items-center bg-slate-600 p-5 max-h-20">
-        <h1 className="text-2xl text-white">
-          <img alt="Proctor" style={{ width: '175px' }} />
-        </h1>
-        <div className="flex items-center justify-end ">
-          <ul className="flex gap-6 list-none mr-8">
-            <li>
-              <a
-                href={location.pathname === '/' ? '#' : '/'}
-                className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/#shop"
-                className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
-              >
-                Shop
-              </a>
-            </li>
-            <li>
-              <a
-                href="/about"
-                className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
-                onClick={(e) => {
-                  navigate('/about');
-                  window.location.reload();
-                }}
-              >
-                About Us
-              </a>
-            </li>
-            <li>
-              <a
-                href="#contactUs"
-                className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
-              >
-                Contact Us
-              </a>
-            </li>
-          </ul>
+    user && (
+      <div>
+        <div className="flex justify-between items-center bg-gradient-to-r from-[#901ee2] to-[#594177] p-5 max-h-20">
+          <h1 className="text-2xl text-white">
+            <img alt="Proctor" style={{ width: '175px' }} />
+          </h1>
+          <div className="flex items-center justify-end ">
+            <ul className="flex gap-6 list-none mr-8">
+              <li>
+                <a
+                  href={location.pathname === '/' ? '#' : '/'}
+                  className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/#shop"
+                  className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
+                >
+                  Shop
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/about"
+                  className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
+                  onClick={(e) => {
+                    navigate('/about');
+                    window.location.reload();
+                  }}
+                >
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#contactUs"
+                  className="text-white text-xl hover:text-gray-600 no-underline hover:bg-gray-200 p-1 hover:rounded"
+                >
+                  Contact Us
+                </a>
+              </li>
+            </ul>
 
-          <div className="bg-white px-2 py-3 rounded flex gap-1 items-center">
-            {/* <i className="ri-user-2-fill"></i>
+            <div className="bg-white px-2 py-3 rounded flex gap-1 items-center">
+              <i className="ri-user-2-fill"></i>
               <span
                 className="underline cursor-pointer"
                 onClick={() => {
-                  if (user.role === 'user') {
-                    navigate('/profile');
-                  } else {
-                    navigate('/admin');
-                  }
+                  navigate('/profile');
                 }}
               >
                 {user.name}
-              </span> */}
+              </span>
 
-            <i
-              className="ri-logout-circle-r-line ml-2 cursor-pointer"
-              onClick={showLogoutConfirmationModal}
-            ></i>
+              <i
+                className="ri-logout-circle-r-line ml-2 cursor-pointer"
+                onClick={showLogoutConfirmationModal}
+              ></i>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <h1>Children Here</h1>
-        {children}
-      </div>
-      <Footer />
-      <ConfirmationModal
+        <div className="container">
+          <h1>Children Here</h1>
+          {children}
+        </div>
+        <Footer />
+        <ConfirmationModal
           visible={showLogoutConfirmation}
           onConfirm={handleLogoutConfirmation}
           onCancel={handleLogoutCancel}
         />
-    </>
+      </div>
+    )
   );
 };
 
--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Spinner = () => {
   console.log('spinner')
   return (
-    <div class="d-flex justify-content-center spinner">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+    <div className="d-flex justify-content-center spinner">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
     </div>
   )
