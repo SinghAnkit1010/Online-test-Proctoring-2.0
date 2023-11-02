465d6284853e4bef00119826a6fa4389fbdff2fe
VivekDevShah <vivek65.ugcs20@iiitranchi.ac.in>
2023-11-02 19:52:03 +0530

    add components

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+
+
+const Layout = ({ children }) => {
+
+
+  return (
+    <>
+      <div className="container">
+        <h1>Children Here</h1>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Layout;
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { hideLoading, showLoading } from '../redux/features/alertSlice'
+import axios from 'axios'
+import { setUser } from '../redux/features/userSlice'
+
+export default function ProtectedRoute({ children }) {
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.user)
+
+  // get user
+  
+  const getUser = async () => {
+    try {
+      dispatch(showLoading())
+      const res = await axios.post(
+        '/api/v1/user/getUserData',
+        { token: localStorage.getItem('token') },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        }
+      )
+      dispatch(hideLoading())
+      if (res.data.success) {
+        dispatch(setUser(res.data.data))
+      } else {
+        dispatch(Navigate('/login')) // Use dispatch here to navigate
+        // <Navigate to= '/login' />
+        localStorage.clear()
+      }
+    } catch (error) {
+      dispatch(hideLoading())
+      localStorage.clear()
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    if (!user) {
+      getUser()
+    }
+  }, [user, getUser])
+
+  if (localStorage.getItem("token")) {
+    return children
+  } else {
+    return <Navigate to="/login" /> // Use <Navigate /> as a statement
+  }
+}
