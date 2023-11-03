5d77dd69f25ff244fbe633e19f56165eb013f593
VivekDevShah <vivek65.ugcs20@iiitranchi.ac.in>
2023-11-04 04:26:18 +0530

    fix bugs of creatTest page

--- client/src/components/QuestionForm.jsx
+++ client/src/components/QuestionForm.jsx
@@ -1,68 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
 import { Form, Input, Button, Select } from "antd";
 
 const { Option } = Select;
 
-const QuestionForm = ({ onSubmit }) => {
-  const [question, setQuestion] = useState("");
-  const [options, setOptions] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState(null);
-
-  const handleQuestionSubmit = (event) => {
-    event.preventDefault();
-
-    // Validate inputs before submitting
-    if (!question || options.length < 2 || !correctAnswer) {
-      return; // Do not submit incomplete questions
-    }
-
-    const questionData = {
-      id: Date.now(),
-      question: question,
-      options: options,
-      correctAnswer: correctAnswer,
-    };
-
-    onSubmit(questionData);
-    
-    // Reset form fields after submitting
-    setQuestion("");
-    setOptions([]);
-    setCorrectAnswer(null);
-  };
-
+const QuestionForm = (props) => {
   return (
-    <Form layout="vertical" onFinish={handleQuestionSubmit}>
-      <Form.Item label="Question" name="question" rules={[{ required: true, message: "Please enter a question!" }]}>
-        <Input value={question} onChange={(e) => setQuestion(e.target.value)} />
+    <Form layout="vertical" form={props.form}>
+      <Form.Item
+        label="Question"
+        name="question"
+        rules={[{ required: true, message: "Please enter a question!" }]}
+      >
+        <Input value={props.question} onChange={props.questionChangeTrig} />
       </Form.Item>
 
-      <Form.Item label="Options" name="options" rules={[{ required: true, message: "Please enter options!" }]}>
-        {options.map((option, index) => (
+      <Form.Item
+        label="Options"
+        name="options"
+        rules={[{ required: true, message: "Please enter options!" }]}
+      >
+        {props.options.map((option, index) => (
           <div key={index}>
-            <Input
-              value={option}
-              onChange={(e) => {
-                const updatedOptions = [...options];
-                updatedOptions[index] = e.target.value;
-                setOptions(updatedOptions);
-              }}
+            <input
+              value={props.options[index]}
+              onChange={(e) => props.optionChangeTrig(index, e)}
             />
-            {index > 1 && (
-              <Button type="link" onClick={() => setOptions(options.filter((_, i) => i !== index))}>
+
+            { (
+              <Button type="link" onClick={() => props.removeOptionTrig(index)}>
                 Remove
               </Button>
             )}
           </div>
         ))}
-        <Button type="dashed" onClick={() => setOptions([...options, ""])} style={{ marginTop: "10px" }}>
+        <Button
+          type="dashed"
+          onClick={() => props.addOptionTrig()}
+          style={{ marginTop: "10px" }}
+        >
           Add Option
         </Button>
       </Form.Item>
 
-      <Form.Item label="Correct Answer" name="correctAnswer" rules={[{ required: true, message: "Please select the correct answer!" }]}>
-        <Select value={correctAnswer} onChange={(value) => setCorrectAnswer(value)}>
-          {options.map((option, index) => (
+      <Form.Item
+        label="Correct Answer"
+        name="correctAnswer"
+        rules={[
+          { required: true, message: "Please select the correct answer!" },
+        ]}
+      >
+        <Select
+          value={props.correctAnswer}
+          onChange={props.changeCorrectAnswerTrig}
+        >
+          {props.options.map((option, index) => (
             <Option key={index} value={index}>
               {`Option ${index + 1}`}
             </Option>
@@ -70,11 +61,13 @@ const QuestionForm = ({ onSubmit }) => {
         </Select>
       </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Add Question
-        </Button>
-      </Form.Item>
+      <Button
+        onClick={props.QuestionSubmitTrig}
+        className="btn btn-primary text-white bg-blue-700 mt-10"
+        type="submit"
+      >
+        Add Question
+      </Button>
     </Form>
   );
 };
--- client/src/pages/CreateTest.jsx
+++ client/src/pages/CreateTest.jsx
@@ -5,21 +5,82 @@ import { Form, Input, message } from "antd";
 import QuestionForm from "../components/QuestionForm";
 
 const CreateTest = () => {
-  const [questions, setQuestions] = useState([]);
+  const [form] = Form.useForm();
+  
+  const [questionSet, setQuestionSet] = useState([]);
 
-  const handleQuestionSubmit = (questionData) => {
-    // Update the questions state with the submitted question data
-    setQuestions([...questions, questionData]);
+  // *********************************************
+  const [question, setQuestion] = useState("");
+
+  const handleQuestionChange = (e) => {
+    setQuestion(e.target.value);
   };
+
+
+
+  const [options, setOptions] = useState([]);
+
+ const handleOptionChange = (index, e) => {
+  const updatedOptions = [...options];
+  updatedOptions[index] = e.target.value;
+  setOptions(updatedOptions);
+};
+
+
+  const handleRemoveOptions = (index) =>
+    setOptions(options.filter((_, i) => i !== index));
+
+  const handleAddOption = () => setOptions([...options, ""]);
+
+
+
+
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const handleChangeCorrectAnswer = (value) => setCorrectAnswer(value);
+
+
+
+
+  const handleQuestionSubmit = (event) => {
+    event.preventDefault();
+
+    // Validate inputs before submitting
+    // if (!question || options.length < 2 ) {
+    //   return; // Do not submit incomplete questions
+    // }
+
+    const questionData = {
+      id: Date.now(),
+      question: question,
+      options: options,
+      correctAnswer: correctAnswer,
+    };
+    console.log(questionData)
+
+    setQuestionSet([...questionSet, questionData]);
+
+    // Reset form fields after submitting
+    setQuestion("");
+    setOptions([]);
+    setCorrectAnswer(null);
+
+    form.resetFields();
+  };
+
+  // ************************************************
 
   const handleTestSubmit = (testData) => {
     // Handle the overall test submission, including the test data and questions array
     // Send this data to your backend API to store in the database
-    console.log('Test Data:', {
-      ...testData,
-      questions: questions,
-    });
-    message.success("sumbitted successfully");
+    try {
+      console.log("Test Data:", {
+        ...testData,
+        questionSet: questionSet,
+      });
+      message.success("sumbitted successfully");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -34,9 +95,57 @@ const CreateTest = () => {
             <Form.Item label="Test Name" name="testName">
               <Input type="text" required />
             </Form.Item>
-            
+            {/* <Form.Item label='Start Date' name='startDate'>
+            <Input
+              type='date' required
+            />
+          </Form.Item>
+          <Form.Item label='Start Time' name='startTime'>
+            <Input
+              type='time' required
+            />
+          </Form.Item>
+          <Form.Item label='Duration (mins)' name='duration'>
+            <Input
+              type='number' required
+            />
+          </Form.Item>
+          <Form.Item label='Number of Questions' name='numQuestions'>
+            <Input
+              type='number' required
+            />
+          </Form.Item> */}
 
-            <QuestionForm onSubmit={handleQuestionSubmit} />
+            {questionSet.length > 0 && (
+              <div className="questions-container">
+                <h2>Questions:</h2>
+                <ul>
+                  {questionSet.map((question, index) => (
+                    <li key={index}>
+                      <strong>Question {index + 1}:</strong> {question.question}
+                      <br />
+                      <strong>Options:</strong> {question.options.join(", ")}
+                      <br />
+                      <strong>Correct Answer:</strong> Option{" "}
+                      {question.correctAnswer + 1}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+            <QuestionForm
+              questionChangeTrig={handleQuestionChange}
+              question={question}
+              options={options}
+              optionChangeTrig={handleOptionChange}
+              removeOptionTrig={handleRemoveOptions}
+              addOptionTrig={handleAddOption}
+              correctAnswer={correctAnswer}
+              changeCorrectAnswerTrig={handleChangeCorrectAnswer}
+              QuestionSubmitTrig={handleQuestionSubmit}
+              form={form}
+            />
 
             <button
               className="btn btn-primary text-white bg-blue-700 mt-10"
