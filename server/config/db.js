1e61115eff22fdb9c87b600ac85a0200a440359b
VivekDev01 <vivek65.ugcs20@iiitranchi.ac.in>
2024-04-28 00:02:09 +0530

    change db configuration

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,33 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+
+    await retryConnecting();
+
     console.log(`MongoDB connected ${mongoose.connection.host}`.bgGreen.white);
   } catch (error) {
     console.log(`MongoDB Server Issue ${error}`.bgRed.white);
+  }
+};
+
+const retryConnecting = async () => {
+  const maxRetryAttempts = 5;
+  let retryCount = 0;
+
+  while (retryCount < maxRetryAttempts) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URL);
+      break; 
+    } catch (error) {
+      retryCount++;
+      console.log(`Connection attempt ${retryCount} failed. Retrying...`);
+
+      if (retryCount < maxRetryAttempts) {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      } else {
+        throw new Error(`Failed to connect to MongoDB after ${maxRetryAttempts} attempts.`);
+      }
+    }
   }
 };
 
